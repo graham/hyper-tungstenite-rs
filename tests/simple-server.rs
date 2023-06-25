d251d02564316220e96092b48dd5ec666405996f
Graham Abbott <graham.abbott@gmail.com>
2023-06-24 20:34:35 -0700

    lib is now correct, working on tests

--- tests/simple-server.rs
+++ tests/simple-server.rs
@@ -1,53 +1,56 @@
-use hyper::{Body, Request, Response};
-use hyper::server::Server;
-use hyper::service::{service_fn, make_service_fn};
-use hyper_tungstenite::tungstenite::Error;
-use tokio::net::TcpStream;
-use std::net::Ipv6Addr;
-use tokio_tungstenite::tungstenite::{Message, Result};
-use futures::stream::StreamExt;
+use bytes::Bytes;
 use futures::sink::SinkExt;
+use futures::stream::StreamExt;
+use http_body_util::Full;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{body::Incoming as IncomingBody, Request, Response};
+use hyper_tungstenite::tungstenite::Error;
+use std::net::Ipv6Addr;
+use tokio::net::TcpStream;
+use tokio_tungstenite::tungstenite::{Message, Result};
 
 use assert2::{assert, let_assert};
+use tokio::net::TcpListener;
 
 #[tokio::test]
 async fn hyper() {
-	// Bind a TCP listener to an ephemeral port.
-	let_assert!(Ok(listener) = std::net::TcpListener::bind((Ipv6Addr::LOCALHOST, 0u16)));
-	let_assert!(Ok(bind_addr) = listener.local_addr());
-	let_assert!(Ok(server) = Server::from_tcp(listener));
+    // Bind a TCP listener to an ephemeral port.
+    let_assert!(Ok(listener) = TcpListener::bind((Ipv6Addr::LOCALHOST, 0u16)).await);
+    let_assert!(Ok(bind_addr) = listener.local_addr());
 
-	// Spawn the server in a task.
-	tokio::spawn(async move {
-		let service = make_service_fn(|_conn| async {
-			Ok::<_, hyper::Error>(service_fn(upgrade_websocket))
-		});
-		let_assert!(Ok(()) = server.http1_only(true).serve(service).await);
-	});
+    tokio::task::spawn(async move {
+        let (stream, _) = listener.accept().await.unwrap();
 
-	// Try to create a websocket connection with the server.
-	let_assert!(Ok(stream) = TcpStream::connect(bind_addr).await);
-	let_assert!(Ok((mut stream, _response)) = tokio_tungstenite::client_async("ws://localhost/foo", stream).await);
+        tokio::task::spawn(async move {
+            http1::Builder::new()
+                .serve_connection(stream, service_fn(upgrade_websocket))
+                .await
+        });
+    });
+    // Try to create a websocket connection with the server.
+    let_assert!(Ok(stream) = TcpStream::connect(bind_addr).await);
+    let_assert!(Ok((mut stream, _response)) = tokio_tungstenite::client_async("ws://localhost/foo", stream).await);
 
-	let_assert!(Some(Ok(message)) = stream.next().await);
-	assert!(message == Message::text("Hello!"));
+    let_assert!(Some(Ok(message)) = stream.next().await);
+    assert!(message == Message::text("Hello!"));
 
-	let_assert!(Ok(()) = stream.send(Message::text("Goodbye!")).await);
-	assert!(let Some(Ok(Message::Close(None))) = stream.next().await);
+    let_assert!(Ok(()) = stream.send(Message::text("Goodbye!")).await);
+    assert!(let Some(Ok(Message::Close(None))) = stream.next().await);
 }
 
-async fn upgrade_websocket(request: Request<Body>) -> Result<Response<Body>> {
-	assert!(hyper_tungstenite::is_upgrade_request(&request) == true);
+async fn upgrade_websocket(mut request: Request<IncomingBody>) -> Result<Response<Full<Bytes>>> {
+    assert!(hyper_tungstenite::is_upgrade_request(&request) == true);
 
-	let (response, stream) = hyper_tungstenite::upgrade(request, None)
-		.map_err(Error::Protocol)?;
-	tokio::spawn(async move {
-		let_assert!(Ok(mut stream) = stream.await);
-		assert!(let Ok(()) = stream.send(Message::text("Hello!")).await);
-		let_assert!(Some(Ok(reply)) = stream.next().await);
-		assert!(reply == Message::text("Goodbye!"));
-		assert!(let Ok(()) = stream.send(Message::Close(None)).await);
-	});
+    let (response, stream) =
+        hyper_tungstenite::upgrade(&mut request, None).map_err(Error::Protocol)?;
+    tokio::spawn(async move {
+        let_assert!(Ok(mut stream) = stream.await);
+        assert!(let Ok(()) = stream.send(Message::text("Hello!")).await);
+        let_assert!(Some(Ok(reply)) = stream.next().await);
+        assert!(reply == Message::text("Goodbye!"));
+        assert!(let Ok(()) = stream.send(Message::Close(None)).await);
+    });
 
-	Ok(response)
+    Ok(response)
 }
